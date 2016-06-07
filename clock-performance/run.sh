2815f0313f9e48313b7af27e644ee68b5f007a1a
Dmitry RCL Rekman <dmitry.rekman@epicgames.com>
2016-06-07 16:14:44 -0400

    Fixed formatting.

--- clock-performance/run.sh
+++ clock-performance/run.sh
@@ -5,14 +5,16 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-echo "NumCores,\tNumIter,\tTotalTime (sec),\tSystemTime (sec),\tUserTime (sec)"
+printf "NumCores,\tNumIter,\tRealTime (sec),\tSystemTime (sec),\tUserTime (sec)\n"
 
 NumIter=10000000
 MaxCores=$(getconf _NPROCESSORS_ONLN)
 
 for NumCores in $(seq 1 $MaxCores); do
-	TimeResults="$(/usr/bin/time --format '%e,\t%S,\t%U' ./clock_performance $NumIter $NumCores 2>&1 1>/dev/null )"
-	echo "$NumCores,\t$NumIter,\t$TimeResults"
+	TimeResults="$(/usr/bin/time --format '%e %S %U' ./clock_performance $NumIter $NumCores 2>&1 1>/dev/null )"
+	printf "%d,\t" $NumCores $NumIter
+	printf "%s,\t" $TimeResults
+	printf "\n"
 done
 
 
